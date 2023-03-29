776baf9c0bbab0ee758666150265c169739f186d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-03-29 10:53:51 +0300

    fix(sign-data): convert ASN.1 sequence

---
+++ src/sign.test.ts
@@ -0,0 +1,20 @@
+import { convertASN1Signature } from './sign';
+
+describe('Sign data', () => {
+  // See: https://asn1.io/asn1playground
+  it('should convert ASN.1 sequence to JWT signature', () => {
+    expect(
+      convertASN1Signature(
+        Buffer.from(
+          '30440220012be13f70f82f6935d1025daa0a8e237b30abff2ea865cfc0f488088831ab7702201de5571337634f3336cd644ccb639ddf49fc470059bed59c6eed146fd4d2b254',
+          'hex',
+        ),
+      ).equals(
+        Buffer.from(
+          '012BE13F70F82F6935D1025DAA0A8E237B30ABFF2EA865CFC0F488088831AB771DE5571337634F3336CD644CCB639DDF49FC470059BED59C6EED146FD4D2B254',
+          'hex',
+        ),
+      ),
+    ).toBe(true);
+  });
+});
--- src/sign.ts
+++ src/sign.ts
@@ -2,6 +2,21 @@ import crypto from 'node:crypto';
 import { crc32c } from '@aws-crypto/crc32c';
 import { KeyManagementServiceClient } from '@google-cloud/kms';
 
+export function convertASN1Signature(data: Uint8Array) {
+  if (
+    !Buffer.from([0x30, 0x44, 0x02, 0x20]).equals(data.slice(0, 4)) ||
+    !Buffer.from([0x02, 0x20]).equals(data.slice(4 + 32, 4 + 32 + 2))
+  ) {
+    throw new Error(
+      'Invalid signature, expected ASN.1 SEQUENCE with 2 INTEGERS',
+    );
+  }
+  return Buffer.concat([
+    data.slice(4, 4 + 32),
+    data.slice(4 + 32 + 2, 4 + 32 + 2 + 32),
+  ]);
+}
+
 export async function signData(
   client: KeyManagementServiceClient,
   {
@@ -30,11 +45,10 @@ export async function signData(
   if (!signResponse.verifiedDigestCrc32c) {
     throw new Error('kmsSign: request corrupted in-transit');
   }
-  if (
-    crc32c(signResponse.signature as Uint8Array) !==
-    Number(signResponse.signatureCrc32c!.value)
-  ) {
+
+  const asn1Signature = signResponse.signature as Uint8Array;
+  if (crc32c(asn1Signature) !== Number(signResponse.signatureCrc32c!.value)) {
     throw new Error('kmsSign: response corrupted in-transit');
   }
-  return signResponse.signature as Uint8Array;
+  return convertASN1Signature(asn1Signature);
 }
