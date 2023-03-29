92c0e053c889da3c4013d5e80abe80949979d137
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-03-29 19:38:15 +0300

    fix(asn1): extract 32-byte signatures properly

--- src/jwt/asn1.test.ts
+++ src/jwt/asn1.test.ts
@@ -1,6 +1,52 @@
-import { convertASN1Signature } from './asn1';
+import { convertASN1Signature, extractSignatureValue } from './asn1';
 
-describe('Sign data', () => {
+describe('ASN.1 signature decoding', () => {
+  describe('extractSignatureValue', () => {
+    it('should zero-fill data in beginning', () => {
+      expect(
+        extractSignatureValue(
+          Buffer.alloc(4).map((_, i) => i + 1),
+          8,
+        ),
+      ).toStrictEqual(
+        Buffer.from([0x00, 0x00, 0x00, 0x00, 0x01, 0x02, 0x03, 0x04]),
+      );
+    });
+
+    it('should return 32 bytes of data', () => {
+      expect(
+        Buffer.from(
+          extractSignatureValue(
+            Buffer.alloc(64).map((_, i) => (i < 32 ? 0 : i)),
+          ),
+        ).toString('hex'),
+      ).toBe(
+        '202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f',
+      );
+    });
+
+    it('should take last n bytes of data', () => {
+      expect(
+        extractSignatureValue(
+          Buffer.alloc(8).map((_, i) => (i < 4 ? 0 : i)),
+          4,
+        ),
+      ).toStrictEqual(Buffer.from([0x04, 0x05, 0x06, 0x07]));
+    });
+
+    it('should throw error on overflow', () => {
+      expect(() =>
+        extractSignatureValue(Buffer.from([0x01, 0x02, 0x03, 0x04, 0x05]), 4),
+      ).toThrow();
+    });
+
+    it('should not throw error if number is not overflowing', () => {
+      expect(
+        extractSignatureValue(Buffer.from([0x00, 0x02, 0x03, 0x04, 0x05]), 4),
+      ).toStrictEqual(Buffer.from([0x02, 0x03, 0x04, 0x05]));
+    });
+  });
+
   // See: https://asn1.io/asn1playground
   it('should convert ASN.1 sequence to JWT signature', () => {
     expect(
@@ -9,12 +55,12 @@ describe('Sign data', () => {
           '30440220012be13f70f82f6935d1025daa0a8e237b30abff2ea865cfc0f488088831ab7702201de5571337634f3336cd644ccb639ddf49fc470059bed59c6eed146fd4d2b254',
           'hex',
         ),
-      ).equals(
-        Buffer.from(
-          '012BE13F70F82F6935D1025DAA0A8E237B30ABFF2EA865CFC0F488088831AB771DE5571337634F3336CD644CCB639DDF49FC470059BED59C6EED146FD4D2B254',
-          'hex',
-        ),
       ),
-    ).toBe(true);
+    ).toStrictEqual(
+      Buffer.from(
+        '012BE13F70F82F6935D1025DAA0A8E237B30ABFF2EA865CFC0F488088831AB771DE5571337634F3336CD644CCB639DDF49FC470059BED59C6EED146FD4D2B254',
+        'hex',
+      ),
+    );
   });
 });
--- src/jwt/asn1.ts
+++ src/jwt/asn1.ts
@@ -1,14 +1,53 @@
+export function extractSignatureValue(data: Uint8Array, size = 32): Uint8Array {
+  if (data.length === size) {
+    return data;
+  }
+  if (data.length > size) {
+    const start = data.length - size;
+    for (let i = 0; i < start; i++) {
+      if (data[i] != 0x00) {
+        throw Error(
+          `Signature overflow: found byte ${data[start]} at position ${start}, integer length is ${data.length}, but it must be ${size}`,
+        );
+      }
+    }
+    return data.slice(start);
+  }
+
+  const result = Buffer.alloc(size, 0x00);
+  let dataIndex = data.length - 1;
+  let resultIndex = result.length - 1;
+  while (dataIndex >= 0) {
+    result[resultIndex] = data[dataIndex];
+    dataIndex -= 1;
+    resultIndex -= 1;
+  }
+  return result;
+}
+
 export function convertASN1Signature(data: Uint8Array) {
-  if (
-    !Buffer.from([0x30, 0x44, 0x02, 0x20]).equals(data.slice(0, 4)) ||
-    !Buffer.from([0x02, 0x20]).equals(data.slice(4 + 32, 4 + 32 + 2))
-  ) {
+  if (data[0] !== 0x30 || data.length < 2) {
+    throw new Error('Invalid signature, expected ASN.1 SEQUENCE');
+  }
+
+  const sequence = data.slice(2, 2 + data[1]);
+  if (sequence[0] !== 0x02 || sequence.length < 2) {
     throw new Error(
-      'Invalid signature, expected ASN.1 SEQUENCE with 2 INTEGERS',
+      'Invalid signature, expected ASN.1 INTEGER within SEQUENCE',
     );
   }
-  return Buffer.concat([
-    data.slice(4, 4 + 32),
-    data.slice(4 + 32 + 2, 4 + 32 + 2 + 32),
-  ]);
+
+  const r = sequence.slice(2, 2 + sequence[1]);
+  const sPosition = 2 + sequence[1];
+  if (sequence[sPosition] !== 0x02 || sequence.length < sPosition + 2) {
+    throw new Error(
+      'Invalid signature, expected two ASN.1 INTEGERs within SEQUENCE',
+    );
+  }
+  const s = sequence.slice(
+    sPosition + 2,
+    sPosition + 2 + sequence[sPosition + 1],
+  );
+
+  return Buffer.concat([extractSignatureValue(r), extractSignatureValue(s)]);
 }
