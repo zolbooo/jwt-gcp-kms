446566bd3ce6012984b0abd0f139fffbff08088f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-03-29 10:13:20 +0300

    feat: export JsonWebKeySet and JsonWebKeyWithID types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { signJWT, verifyJWT } from './jwt.js';
 export { getPublicKeys } from './public-keys.js';
 
-export type { KeyPath } from './types';
+export type { KeyPath, JsonWebKeySet, JsonWebKeyWithID } from './types';
