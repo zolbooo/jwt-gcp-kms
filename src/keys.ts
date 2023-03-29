93fa64fd0bad16037676ad6ff60ed77ada2b4359
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-03-29 09:06:31 +0300

    feat: add getLatestVersion function

---
+++ src/keys.ts
@@ -0,0 +1,21 @@
+import { KeyManagementServiceClient } from '@google-cloud/kms';
+
+export async function getLatestVersion(
+  client: KeyManagementServiceClient,
+  keyName: string,
+): Promise<{ name: string }> {
+  const [versions] = await client.listCryptoKeyVersions({
+    parent: keyName,
+    filter: 'state=ENABLED',
+  });
+  let latestVersion = versions[0];
+  for (let i = 1; i < versions.length; i += 1) {
+    if (
+      new Date(latestVersion.createTime as string).valueOf() <
+      new Date(versions[i].createTime as string).valueOf()
+    ) {
+      latestVersion = versions[i];
+    }
+  }
+  return { name: latestVersion.name as string };
+}
