50d46b3a1ebf7618c75d9f10c826e9bef9668b9a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-01 21:44:17 +0300

    fix(verify-jwt): return type

--- src/jwt/verify.ts
+++ src/jwt/verify.ts
@@ -1,13 +1,20 @@
-import jwt, { JwtPayload, VerifyOptions } from 'jsonwebtoken';
+import jwt, { Jwt, JwtPayload, VerifyOptions } from 'jsonwebtoken';
 import crypto from 'node:crypto';
 
 import type { JsonWebKeySet } from '../types';
 
+/**
+ * Verify JWT against specific key set.
+ * @param token JWT to verify
+ * @param jwks Public key set in JWKS format. Can be retrieved using `getPublicKeys` function.
+ * @param options Some additional options provided by [jsonwebtoken](https://github.com/auth0/node-jsonwebtoken) library
+ * @returns JWT payload, or complete token data/token itself if `complete` option was set.
+ */
 export function verifyJWT(
   token: string,
   jwks: JsonWebKeySet,
   options?: VerifyOptions,
-): JwtPayload {
+): JwtPayload | Jwt | string {
   const rawTokenData = jwt.decode(token, { complete: true });
   if (!rawTokenData?.header.kid) {
     throw Error('No kid provided in token');
@@ -23,5 +30,5 @@ export function verifyJWT(
     key: publicKeyJwk,
     format: 'jwk',
   });
-  return jwt.verify(token, publicKey, options) as JwtPayload;
+  return jwt.verify(token, publicKey, options);
 }
