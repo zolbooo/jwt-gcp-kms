d25782ca884751991af4d6a828da68ae4eb1e6dd
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-01 20:14:40 +0300

    test(e2e): configure ESM modules

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,8 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
+  extensionsToTreatAsEsm: ['.ts'],
   preset: 'ts-jest',
   testRegex: '.e2e-spec.ts$',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', { useESM: true }],
   },
 };
