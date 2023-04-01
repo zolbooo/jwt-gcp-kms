75bbe201111c2c008d7c2d48f2981ce07fa8de5d
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2023-04-01 20:51:29 +0300

    refactor: move timestampToDate function to separate module

--- src/keys.ts
+++ src/keys.ts
@@ -1,17 +1,6 @@
 import { KeyManagementServiceClient } from '@google-cloud/kms';
 
-import type { google } from '@google-cloud/kms/build/protos/protos';
-
-function timestampToDate(timestamp?: google.protobuf.ITimestamp | null): Date {
-  if (!timestamp) {
-    return new Date(0);
-  }
-  return new Date(
-    typeof timestamp.seconds === 'object'
-      ? (timestamp.seconds?.toNumber() ?? 0) * 1000
-      : Number(timestamp.seconds ?? 0) + Number(timestamp.nanos ?? 0) / 1000000,
-  );
-}
+import { timestampToDate } from './utils/timestamp';
 
 export async function getLatestVersion(
   client: KeyManagementServiceClient,
